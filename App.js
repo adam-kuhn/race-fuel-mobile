70eeb0410265a8521ed5f38d5247ae38b32a9e74
adam-kuhn <adam.kuhn20@gmail.com>
2018-07-27 20:03:14 -0600

    add support of all react-native-vector-icons for Android

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {StyleSheet, Text, View} from 'react-native'
+import Icon from 'react-native-vector-icons/Octicons'
 
 export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
         <Text>This will be Race Fuel for Mobile</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Icon name='dashboard' size={30} color='blue' />
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     )
